08053168f913bc15754727d657f06e4ab766acf2
Nana Gohil <gohilnana9@gmail.com>
2024-08-21 17:05:17 +0530

    second commit

--- lib/utils/global_variable.dart
+++ lib/utils/global_variable.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:friendsphere/screens/add_post_screen.dart';
 const webSreenSize = 600;
 const HomeScreenItems =[
-
   Text("search"),
   AddPostScreen(),
   Text("notif"),
